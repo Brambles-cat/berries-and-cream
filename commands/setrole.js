c2f5c5ae27718ef53d015a3e0025b84cf7a737ce
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2021-12-31 09:49:42 -0800

    Update setrole.js

--- commands/setrole.js
+++ commands/setrole.js
@@ -1,12 +1,17 @@
 module.exports = {
 	name: 'setrole',
 	execute(message, args) {
-		message.channel.send(args[0].slice(3, -1));
-		if (!(message.mentions.roles.first()) || args[0].slice(3, -1) != message.mentions.roles.first().id) return;
-		message.client.usingData(data => {
+		if (!(message.mentions.roles.first()) || args[0].slice(3, -1) != message.mentions.roles.first().id) {
+			message.reply('Not a valid role (it must be mentioned instead of the name being typed)');
+			return;
+		}
+		message.client.usingData.then(data => {
 			data.role = args[0].slice(3, -1);
 			message.client.writeData(data);
-			message.channel.send('set <@&' + data.role + '> as role');
+			message.channel.send('set <@&' + data.role + '> as role to be pinged');
+		}, reject => {
+			console.log(reject);
+			message.channel.send('An error has occured');
 		});
 	},
 };
